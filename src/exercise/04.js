ccd6815fe7c818e8151df5a37cdb1f8b2648f90c
Ching-Ju Hsieh <chingjshieh@gmail.com>
2021-04-06 01:58:00 +0800

    feat(04): props getter

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,25 +2,33 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
 
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
+  const togglerProps = { 'aria-pressed': on, onClick: toggle };
 
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return { on, togglerProps }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const { on, togglerProps } = useToggle()
+  const ariaPressedProp = togglerProps["aria-pressed"];
+  const togglerClick = togglerProps.onClick;
   return (
     <div>
       <Switch on={on} {...togglerProps} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button aria-label="custom-button"
+        aria-pressed={ariaPressedProp}
+        onClick={() => {
+          togglerClick()
+          console.info('onButtonClick')
+        }}>
         {on ? 'on' : 'off'}
       </button>
     </div>
